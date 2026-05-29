a41653421ea97e56427ff70824ed3e9b67b1179f
Lukas Lammers <llammer3@uni-koeln.de>
2026-05-29 15:53:48 +0200

    Refactor country info module for improved functionality
    Refactor get_country_name and build_country_xml functions to include error handling and support multiple country codes. Update XML structure and add process_country_codes function for batch processing.

--- modules/wikidata_country_info.py
+++ modules/wikidata_country_info.py
@@ -4,41 +4,80 @@
 
 #get country name from wikidata api
 def get_country_name(wikidata_code):
+    headers = {"User-Agent": "MTT/1.0 (lukas.lammers@uni-koeln.de)"}
     wikidata_code = wikidata_code.upper()
-    url = f"https://query.wikidata.org/sparql?query=SELECT%20%3Fcountry%20%3FcountryLabel%20WHERE%20%7B%20%3Fcountry%20wdt%3AP298%20%22{wikidata_code}%22%20.%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20%7D%20%7D&format=json"
-    response = requests.get(url)
+    url = "https://query.wikidata.org/sparql"
+
+    query = """
+    SELECT ?country ?countryLabel WHERE {
+    ?country wdt:P298 "%s".
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    }
+    """ % wikidata_code
+
+    response = requests.get(
+        url,
+        params={"query": query, "format": "json"},
+        headers=headers
+    )
+    #print(response)
     if response.status_code == 200:
         data = response.json()
-        q_id = data['results']['bindings'][0]['country']['value'].split('/')[-1]
-        country_name = data['results']['bindings'][0]['countryLabel']['value']
-        name_language = data['results']['bindings'][0]['countryLabel']['xml:lang']
-        return q_id, country_name, name_language
+        #print(data)
+        bindings = data.get('results', {}).get('bindings', [])
+        if not bindings:
+            q_id = "Unknown"
+            country_name = "Unknown"
+            name_language = "Unknown"
+            wikidata_code = wikidata_code
+            return q_id, country_name, name_language, wikidata_code
+
+        q_id = bindings[0]['country']['value'].split('/')[-1]
+        country_name = bindings[0]['countryLabel']['value']
+        name_language = bindings[0]['countryLabel']['xml:lang']
+        return q_id, country_name, name_language, wikidata_code
     else:
         q_id = "Unknown"
         country_name = "Unknown"
         name_language = "Unknown"
-        return q_id, country_name, name_language
+        wikidata_code = wikidata_code
+        return q_id, country_name, name_language, wikidata_code
 
 #q_id, country_name, name_language = get_country_name(wikidata_country_code)
 #print(f"The country name for Wikidata code '{wikidata_country_code}' is: {country_name} in language '{name_language}'")
 
-def build_country_xml(q_id, country_name, name_language):
-    first_row = f'''<rdf:Description rdf:about="http://www.wikidata.org/entity/{q_id}">'''
-    second_row = f'''<rdfs:label xml:lang="{name_language}">{country_name}</rdfs:label>'''
-    third_row = '''</rdf:Description>'''
-    country_xml = f"{first_row}\n{second_row}\n{third_row}"
-    return country_xml
+def build_country_xml(q_id, country_name, name_language, wikidata_code):
+    if q_id == "Unknown" or country_name == "Unknown" or name_language == "Unknown":
+        return None
+    else:
+        first_row = '''<dch:wikidata_country>'''
+        second_row = f'''<dch:wikidata_country_url>http://www.wikidata.org/entity/{q_id}</dch:wikidata_country_url>'''
+        third_row = f'''<rdfs:label xml:lang="{name_language}">{country_name}</rdfs:label>'''
+        fourth_row = '''</dch:wikidata_country>'''
+        country_xml = f"{first_row}\n{second_row}\n{third_row}\n{fourth_row}"
+        return country_xml
+    
+def process_country_codes(codestring):
+    codes = codestring.split(", ")
+    results = []
+    for code in codes:
+        q_id, country_name, name_language, wikidata_code = get_country_name(code)
+        country_xml = build_country_xml(q_id, country_name, name_language, wikidata_code)
+        if country_xml:
+            results.append(country_xml)
+    #Convert list of XML snippets into a single string
+    results_xml = "".join(results)
+    return results_xml
 
-#country_xml = build_country_xml(q_id, country_name, name_language)
-#print(country_xml)
 
 if __name__ == "__main__":
     import requests
     #test with this 3-letter Wikipedia ISO 3166
-    wikidata_country_code = "EGY"
-    q_id, country_name, name_language = get_country_name(wikidata_country_code)
-    print(f"The country name for Wikidata code '{wikidata_country_code}' is: {country_name} in language '{name_language}'")
-    country_xml = build_country_xml(q_id, country_name, name_language)
-    print(country_xml)
+    wikidata_country_code = "EGY, RWA, LBY, -99"
+    #q_id, country_name, name_language = get_country_name(wikidata_country_code)
+    #print(f"The country name for Wikidata code '{wikidata_country_code}' is: {country_name} in language '{name_language}'")
+    #country_xml = build_country_xml(q_id, country_name, name_language)
+    #print(country_xml)
+    print(process_country_codes(wikidata_country_code))
 else:
     import requests
