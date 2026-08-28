cbd157b0833ebe68122a53fe54d2fd5404712baa
Lukas Lammers <llammer3@uni-koeln.de>
2026-08-28 11:43:53 +0200

    Update mtt_config.py with new variables and URLs

--- modules/mtt_config.py
+++ modules/mtt_config.py
@@ -4,17 +4,30 @@
 
 #Please use the tables in templates folder to set constant values for all metadata.
 
+#2. LANGUAGES
+#You can use this list to set the languages in the dropdown menu.
+languages = ["http://www.lexvo.org/page/iso639-3/deu", "http://www.lexvo.org/page/iso639-3/eng"]
+
 #2. GIT SYNC VARIABLES
 
 local_folder="metadata_mirror"
-repo_url="https://somekindofgit.de/something.git"
+repo_url="https://gitlab.dh.uni-koeln.de/your_repo.git"
 target_subdir="test_metadata"
-token="enter-your-git-api-token-here"
+token="your_token_here"
 branch="main"
 
 #3. ONLINE INFRASTRUCTURE VARIABLES
 
-git_repo = "https://somekindofgit.de/repo"
-oai_pmh_status = "https://oai.yourdomain/admin/data-provider.do"
-oai_pmh_list = "http://oai.yourdomain/provider?verb=ListRecords&metadataPrefix=oai_dc"
-ariadne = "https://portal.ariadne-infrastructure.eu/"
+getty_aat = "https://www.getty.edu/research/tools/vocabularies/aat/"
+ariadne_portal = "https://portal.ariadne-infrastructure.eu/"
+staging_portal = "https://ariadne-portal-staging.d4science.org/"
+staging_graph_db = "https://ariadne-graphdb-test.cloud.d4science.org/graphs"
+chronontology = "https://chronontology.dainst.org/"
+periodo = "https://client.perio.do/?page=open-backend"
+ao_cat = "https://zenodo.org/records/21476199"
+lexvo = "http://www.lexvo.org/"
+git_repo = "https://gitlab.dh.uni-koeln.de/your_repo.git"
+oai_pmh_status = "https://oai.dch.phil-fak.uni-koeln.de/admin/data-provider.do"
+oai_pmh_list = "http://oai.dch.phil-fak.uni-koeln.de/provider?verb=ListRecords&metadataPrefix=oai_dc"
+three_m = "https://demos.isl.ics.forth.gr/3m/"
+git_hub = "https://github.com/Data-Center-for-the-Humanities/SPP2143-Metadata-Tables-Tool"
